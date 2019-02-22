9de4806ee8e8f16a108b6c28519fc4ddfa32ca3e
Aedan <aedan.giffin@gmail.com>
2019-02-22 03:57:46 +0000

    Secure admin only pages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def confirmation_email(user, order)
     @user = user
     @order = order
-    mail(to: @user.email, subject: "Your Order: #{order.id}")
+    mail(to: @order.email, subject: "Your Order: #{order.id}")
   end
 
 end
